922f89ae1fe6afba9708bed94fa4a4daefcf830a
Vindya Perera <40080989+vindiuom96@users.noreply.github.com>
2023-05-21 15:45:57 +1000

    Update format_csv.py

--- format_csv.py
+++ format_csv.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
-df1 = pd.read_csv("CSV/entities_exported.csv")
-print(df1['/name'])
 def format_csv():
-    global df1
+    df1 = pd.read_csv("CSV/entities_exported.csv")
     df2 = df1[df1['/name'].isna()]
     df1 = df1.drop(df2.index.tolist())
     df2 = df1[df1['/acceptedAnswer/text'].isna()]
     df1 = df1.drop(df2.index.tolist())
-    print(df1['/name'])
     return df1
