82035a756366b731b093769aab3fad366105efe3
Vindya Perera <40080989+vindiuom96@users.noreply.github.com>
2023-05-21 15:47:18 +1000

    Update updateCSV.py

--- updateCSV.py
+++ updateCSV.py
@@ -2,9 +2,10 @@ import csv
 
 def add_new_question(question , answer):
     with open("CSV/entities_exported.csv", "a") as file:
-        feildNames = ["/@type","/acceptedAnswer/@type","/name", "/acceptedAnswer/text"]
+        feildNames = ["/@type","/acceptedAnswer/@type","/acceptedAnswer/text","/name"]
         writter = csv.DictWriter(file, fieldnames=feildNames)
-        writter.writerow({"/@type":"Question","/acceptedAnswer/@type":"Answer","/name": question, "/acceptedAnswer/text": answer})
+        writter.writerow({"/@type":"Question","/acceptedAnswer/@type":"Answer","/acceptedAnswer/text": answer,"/name": question})
     return
+
 
 
